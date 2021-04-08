647db756edbcb0e3b1eb9e732d97146cf3f38d44
jliu237 <jliu237@ucsc.edu>
2021-04-07 23:06:04 -0700

    addi and display assets

--- src/scences/Play.js
+++ src/scences/Play.js
@@ -2,13 +2,21 @@ class Play extends Phaser.Scene{
     constructor(){
         super("playScene")
     }
+    preload(){
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/rocket.png');
+        this.load.image('starfield', './assets/starfield.png');
+
+    }
     create(){
         this.add.rectangle(0, borderUISize * 2, game.config.width, borderUISize * 2,0x00FF00).setOrigin(0,0);
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
-
+    }
+    update(){
+        this.starfield.tilePositionX -= 4;
 
     }
 
