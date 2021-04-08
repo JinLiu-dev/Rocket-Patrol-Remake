6220b9bd71f265975c5434d31e4cc9551c9a82f1
jliu237 <jliu237@ucsc.edu>
2021-04-07 23:00:49 -0700

    index background color

--- src/scences/Menu.js
+++ src/scences/Menu.js
@@ -1,3 +1,10 @@
-class Scene extends Phaser.Scene{
-    
+class Menu extends Phaser.Scene{
+    constructor(){
+        super("menuScene")
+    }
+    create(){
+        this.add.text(20, 20, "Rocket Patrol Menu");
+        this.scene.start("playScene");
+    }
+
 }
