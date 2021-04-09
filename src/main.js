85071c4d80465af44e58d53a9442f3341d41b9ac
jliu237 <jliu237@ucsc.edu>
2021-04-09 13:51:40 -0700

    add rocket and keyboard control

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ let config = {
     height: 480,
     scene:[Menu, Play],
 }
+let keyF, keyR, keyLEFT, keyRIGHT;
 let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
